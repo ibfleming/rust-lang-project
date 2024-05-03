41cafab95d5055977fe196384d9603dce08c7aac
ibfleming <ianfleming678@gmail.com>
2024-05-02 22:49:24 -0700

    Making a Unit Conversion Application in Rust:

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,78 @@
-trait Animal {
-    fn make_sound(&self);
+/*
+
+    Unit Converter
+
+    Temperature:
+    Celius <-> Fahrenheit <-> Kelvin
+
+    Time:
+    Seconds <-> Minutes <-> Hours <-> Days <-> Weeks <-> Months <-> Years
+
+    Length:
+    Millimeters <-> Centimeters <-> Meters <-> Kilometers <-> Inches <-> Feet <-> Yards <-> Miles
+
+    Mass:
+    Milligrams <-> Grams <-> Kilograms <-> Ounces <-> Pounds <-> Stones
+
+    US Dollar:
+    Cents <-> Dollars
+
+    Currency:
+    Dollars <-> Euros <-> Pounds <-> Yen <-> Yuan <-> Rupees <-> Rubles <-> Pesos <-> Ringgit <-> Rupees
+
+*/
+
+enum UnitType {
+    Temperature,
+    Time,
+    Length,
+    Mass,
+    Currency,
 }
 
-impl Animal for Dog {
-    fn make_sound(&self) {
-        println!("Bark!");
-    }
+enum Unit {
+    Celsius,
+    Fahrenheit,
+    Kelvin,
 }
 
-struct Dog;
-
-trait Cat: Animal {
-    fn purr(&self);
-}
-
-struct Moesha;
-
-impl Animal for Moesha {
-    fn make_sound(&self) {
-        println!("Meow!");
-    }
-}
-
-impl Cat for Moesha {
-    fn purr(&self) {
-        println!("Purr!");
-    }
-}
-
-fn describe(animal: &dyn Animal) {
-    animal.make_sound();
-}
+use UnitType::*;
+use Unit::*;
 
 fn main() {
-    let dog = Dog;
-    let moesha = Moesha;
 
-    describe(&dog);
+}
 
-    moesha.make_sound();
-    moesha.purr();
+/// Temperature Conversion Function
+///
+/// # Parameters
+///
+/// * `a` - From temperature value of conversion.
+/// * `a_unit` - Unit of the `a` temperature value.
+/// * `b` - To temperature value of conversion.
+///  * `b_unit` - Unit of the 'b' temperature value.
+/// 
+/// # Returns
+///
+/// * `f64` - Final value of the conversion
+///
+/// ```
+fn temperature_conversion(a: f64, from_unit: Unit, to_unit: Unit) -> f64 {
+
+    match from_unit {
+        Celsius => {
+            match to_unit {
+                Celsius => {
+                    println!("Both units are the same.");
+                    return a;
+                },
+                Fahrenheit => return (a * 9.0 / 5.0) + 32.0,
+                Kelvin => return a + 273.15,
+            }
+        },
+        Fahrenheit => todo!(),
+        Kelvin => todo!(),
+    }
+
+    return 0.0;
 }
