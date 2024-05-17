f1c5513e0a13e6e712924f486618116c339c296b
ibfleming <ianfleming678@gmail.com>
2024-05-16 17:48:18 -0700

    Got temperature conversion down.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 pub enum UnitType {
     Temperature,
     Time,
@@ -6,12 +7,14 @@ pub enum UnitType {
     Currency,
 }
 
+#[derive(Debug, PartialEq)]
 pub enum TemperatureUnits {
     Celsius,
     Fahrenheit,
     Kelvin,
 }
 
+#[derive(Debug, PartialEq)]
 pub enum TimeUnits {
     Seconds,
     Minutes,
@@ -22,8 +25,21 @@ pub enum TimeUnits {
     Years,
 }
 
+#[derive(Debug, PartialEq)]
+pub enum LengthUnits {
+    Millimeters,
+    Centimeters,
+    Meters,
+    Kilometers,
+    Inches,
+    Feet,
+    Yards,
+    Miles,
+}
+
 use TemperatureUnits::*;
 use TimeUnits::*;
+use LengthUnits::*;
 
 /// Temperature Conversion Function
 ///
@@ -192,3 +208,144 @@ pub fn time_conversion(a: f64, from_unit: TimeUnits, to_unit: TimeUnits) -> f64
         },
     }
 }
+
+/// Length Conversion Function
+///
+/// # Parameters
+///
+/// * `a` - From length value of conversion.
+/// * `a_unit` - Unit of the `a` length value.
+/// * `b` - To length value of conversion.
+///  * `b_unit` - Unit of the 'b' length value.
+/// 
+/// # Returns
+///
+/// * `f64` - Final value of the conversion
+///
+/// '''
+pub fn length_conversion(a: f64, from_unit: LengthUnits, to_unit: LengthUnits) -> f64
+{
+    match from_unit {
+        Millimeters => {
+            match to_unit {
+                Millimeters => {
+                    println!("Both units are the same.");
+                    return a;
+                },
+                Centimeters => return a / 10.0,
+                Meters => return a / 1000.0,
+                Kilometers => return a / 1000000.0,
+                Inches => return a / 25.4,
+                Feet => return a / 304.8,
+                Yards => return a / 914.4,
+                Miles => return a / 1609344.0,
+            }
+        },
+        Centimeters => {
+            match to_unit {
+                Millimeters => return a * 10.0,
+                Centimeters => {
+                    println!("Both units are the same.");
+                    return a;
+                },
+                Meters => return a / 100.0,
+                Kilometers => return a / 100000.0,
+                Inches => return a / 2.54,
+                Feet => return a / 30.48,
+                Yards => return a / 91.44,
+                Miles => return a / 160934.4,
+            }
+        },
+        Meters => {
+            match to_unit {
+                Millimeters => return a * 1000.0,
+                Centimeters => return a * 100.0,
+                Meters => {
+                    println!("Both units are the same.");
+                    return a;
+                },
+                Kilometers => return a / 1000.0,
+                Inches => return a * 39.3701,
+                Feet => return a * 3.28084,
+                Yards => return a * 1.09361,
+                Miles => return a / 1609.34,
+            }
+        },
+        Kilometers => {
+            match to_unit {
+                Millimeters => return a * 1000000.0,
+                Centimeters => return a * 100000.0,
+                Meters => return a * 1000.0,
+                Kilometers => {
+                    println!("Both units are the same.");
+                    return a;
+                },
+                Inches => return a * 39370.1,
+                Feet => return a * 3280.84,
+                Yards => return a * 1093.61,
+                Miles => return a / 1.60934,
+            }
+        },
+        Inches => {
+            match to_unit {
+                Millimeters => return a * 25.0,
+                Centimeters => return a * 2.54,
+                Meters => return a / 39.3701,
+                Kilometers => return a / 39370.1,
+                Inches => {
+                    println!("Both units are the same.");
+                    return a;
+                },
+                Feet => return a / 12.0,
+                Yards => return a / 36.0,
+                Miles => return a / 63360.0,
+            }
+        },
+        Feet => {
+            match to_unit {
+                Millimeters => return a * 304.8,
+                Centimeters => return a * 30.48,
+                Meters => return a / 3.28084,
+                Kilometers => return a / 3280.84,
+                Inches => return a * 12.0,
+                Feet => {
+                    println!("Both units are the same.");
+                    return a;
+                },
+                Yards => return a / 3.0,
+                Miles => return a / 5280.0,
+            }
+        },
+        Yards => {
+            match to_unit {
+                Millimeters => return a * 914.4,
+                Centimeters => return a * 91.44,
+                Meters => return a / 1.09361,
+                Kilometers => return a / 1093.61,
+                Inches => return a * 36.0,
+                Feet => return a * 3.0,
+                Yards => {
+                    println!("Both units are the same.");
+                    return a;
+                },
+                Miles => return a / 1760.0,
+            }
+        },
+        Miles => {
+            match to_unit {
+                Millimeters => return a * 1609344.0,
+                Centimeters => return a * 160934.4,
+                Meters => return a * 1609.34,
+                Kilometers => return a * 1.60934,
+                Inches => return a * 63360.0,
+                Feet => return a * 5280.0,
+                Yards => return a * 1760.0,
+                Miles => {
+                    println!("Both units are the same.");
+                    return a;
+                },
+            }
+        }
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -23,8 +23,104 @@
 */
 
 use unit_converter::*;
+use unit_converter::UnitType::*;
 use unit_converter::TemperatureUnits::*;
+use std::io::{self, Write};
+
+const DEGREE_SYMBOL: char = '\u{00B0}';
 
 fn main() {
-    temperature_conversion(64.0, Fahrenheit, Kelvin);
+
+    // Input
+    let mut input = String::new();
+    print!("> ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let input: Vec<&str> = input.split_whitespace().collect();
+    if input.len() != 3 { return }
+    
+    if is_f64(input[0]) {
+
+        let val = input[0].parse::<f64>().unwrap();
+        let from_unit = is_valid_unit(input[1]);
+        let to_unit = is_valid_unit(input[2]);
+
+        if from_unit == None || to_unit == None { return }
+        if from_unit != to_unit { return }
+
+        //println!("{:?} {:?} {:?}", val, from_unit.unwrap(), to_unit.unwrap());
+
+        match from_unit {
+            Some(Temperature) => {
+                let from_unit = get_temp_unit(input[1]);
+                let to_unit = get_temp_unit(input[2]);
+                if from_unit == None {
+                    println!("Invalid Left unit.");
+                    return;
+                }
+                if to_unit == None {
+                    println!("Invalid Right unit.");
+                    return;
+                }
+                match to_unit {
+                    Some(Celsius) => println!("{:.2}{}C", temperature_conversion(val, from_unit.unwrap(), to_unit.unwrap()), DEGREE_SYMBOL),
+                    Some(Fahrenheit) => println!("{:.2}{}F", temperature_conversion(val, from_unit.unwrap(), to_unit.unwrap()), DEGREE_SYMBOL),
+                    Some(Kelvin) => println!("{:.2} K", temperature_conversion(val, from_unit.unwrap(), to_unit.unwrap())),
+                    _ => return,
+                }
+            },
+            Some(Time) => {
+            },
+            Some(Length) => {
+            },
+            Some(Currency) => {
+            },
+            Some(Mass) => {
+            },
+            None => return,
+        }
+
+    }
 }
+
+fn is_f64(x: &str) -> bool {
+    match x.parse::<f64>() {
+        Ok(_) => return true,
+        Err(_) => return false,
+    }
+}
+
+fn is_valid_unit(s: &str) -> Option<UnitType> {
+    match s.to_lowercase().as_str() {
+        "c" | "celsius" => return Some(Temperature),
+        "f" | "fahrenheit" => return Some(Temperature),
+        "k" | "kelvin" => return Some(Temperature),
+        "s" | "seconds" => return Some(Time),
+        "min" | "minutes" => return Some(Time),
+        "h" | "hours" => return Some(Time),
+        "d" | "days" => return Some(Time),
+        "w" | "weeks" => return Some(Time),
+        "mo" | "months" => return Some(Time),
+        "y" | "years" => return Some(Time),
+        "mm" | "millimeters" => return Some(Length),
+        "cm" | "centimeters" => return Some(Length),
+        "m" | "meters" => return Some(Length),
+        "km" | "kilometers" => return Some(Length),
+        "in" | "inches" => return Some(Length),
+        "ft" | "feet" => return Some(Length),
+        "yd" | "yards" => return Some(Length),
+        "mi" | "miles" => return Some(Length),
+        _ => None,
+    }
+}
+
+fn get_temp_unit(s: &str) -> Option<TemperatureUnits> {
+    match s.to_lowercase().as_str() {
+        "c" | "celsius" => return Some(Celsius),
+        "f" | "fahrenheit" => return Some(Fahrenheit),
+        "k" | "kelvin" => return Some(Kelvin),
+        _ => None,
+    }
+}
+
